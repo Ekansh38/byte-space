a5ecad318fac6f4f58413118f12578fdfba9268d
Ekansh Goenka <ekanshgoenka@Byte-Colony-Main.local>
2026-03-19 18:14:00 +0800

    feat(shell): added mkdir and rm

--- engine/shell.go
+++ engine/shell.go
@@ -1,10 +1,11 @@
 package engine
 
 import (
-	"byte-space/utils"
 	"fmt"
 	"path"
 	"strings"
+
+	"byte-space/utils"
 
 	"github.com/spf13/afero"
 )
@@ -24,7 +25,6 @@ func (s *Shell) Run(command string) *EngineIPCMessage {
 	}
 
 	return s.RunCommand(commandP)
-
 }
 
 func (s *Shell) RunCommand(command []string) *EngineIPCMessage {
@@ -37,16 +37,18 @@ func (s *Shell) RunCommand(command []string) *EngineIPCMessage {
 		return s.pwd(command)
 	case "whoami":
 		return s.whoami(command)
+	case "mkdir":
+		return s.mkdir(command)
+	case "rm":
+		return s.rm(command)
 	default:
 		return newIPCMessage("not implemented", utils.Warning)
 	}
-
 }
 
 func (s *Shell) ls(commandParsed []string) *EngineIPCMessage {
 	lsDir := s.Session.WorkingDir
 	output := ""
-
 	if len(commandParsed) > 1 {
 		for _, arg := range commandParsed[1:] {
 			if strings.HasPrefix(arg, "-") {
@@ -60,6 +62,7 @@ func (s *Shell) ls(commandParsed []string) *EngineIPCMessage {
 		}
 	}
 
+	lsDir = s.expandPath(lsDir)
 
 	files, err := afero.ReadDir(s.Session.Computer.Filesystem, lsDir)
 	if err != nil {
@@ -81,11 +84,9 @@ func (s *Shell) ls(commandParsed []string) *EngineIPCMessage {
 	}
 
 	return newIPCMessage(output, utils.Success)
-
 }
 
 func (s *Shell) cd(commandParsed []string) *EngineIPCMessage {
-
 	if len(commandParsed) == 1 {
 		return newIPCMessage("No directory provided", utils.Error)
 	}
@@ -102,7 +103,6 @@ func (s *Shell) cd(commandParsed []string) *EngineIPCMessage {
 	}
 
 	dir = path.Clean(dir)
-
 
 	_, err := afero.ReadDir(s.Session.Computer.Filesystem, dir)
 	if err != nil {
@@ -123,7 +123,6 @@ func parseCommand(command string) []string {
 	return strings.Fields(command)
 }
 
-
 func (s *Shell) pwd(commandParsed []string) *EngineIPCMessage {
 	if len(commandParsed) > 1 {
 		return newIPCMessage("pwd: too many arguments", utils.Error)
@@ -142,4 +141,138 @@ func (s *Shell) whoami(commandParsed []string) *EngineIPCMessage {
 		Result: s.Session.CurrentUser,
 		Status: utils.Success,
 	}
+}
+
+func (s *Shell) mkdir(commandParsed []string) *EngineIPCMessage {
+	if len(commandParsed) != 2 {
+		return newIPCMessage("mkdir: missing operand", utils.Error)
+	}
+	for _, arg := range commandParsed[1:] {
+		if strings.HasPrefix(arg, "-") {
+			return newIPCMessage("Flags not supported yet", utils.Error)
+		}
+	}
+
+	dir := commandParsed[1]
+
+	if !strings.HasPrefix(dir, "/") {
+		dir = path.Join(s.Session.WorkingDir, dir)
+	}
+
+	dir = path.Clean(dir)
+
+	if s.directoryExistsCaseSensitive(dir) {
+		return newIPCMessage("mkdir: cannot create directory: File exists", utils.Error)
+	}
+
+	err := s.Session.Computer.Filesystem.Mkdir(dir, 0o755)
+	if err != nil {
+		message := "Failed to create directory"
+		if strings.HasSuffix(err.Error(), "no such file or directory") {
+			message = "mkdir: cannot create directory: No such file or directory"
+		}
+
+		return newIPCMessage(message, utils.Error)
+	}
+
+	return newIPCMessage("", utils.SuccessDoNotDisplay)
+}
+
+func (s *Shell) directoryExistsCaseSensitive(targetPath string) bool {
+	parentDir := path.Dir(targetPath)
+	targetName := path.Base(targetPath)
+
+	if targetPath == "/" {
+		return true
+	}
+
+	files, err := afero.ReadDir(s.Session.Computer.Filesystem, parentDir)
+	if err != nil {
+		return false
+	}
+
+	for _, file := range files {
+		if file.Name() == targetName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (s *Shell) rm(commandParsed []string) *EngineIPCMessage {
+	if len(commandParsed) > 3 {
+		return newIPCMessage("rm: too many arguments", utils.Error)
+	}
+
+	// Flags
+
+	flags := make(map[string]bool)
+	numTargets := 0
+	target := ""
+	if len(commandParsed) >= 2 {
+		for _, arg := range commandParsed[1:] {
+			if strings.HasPrefix(arg, "-") {
+				for _, flag := range arg[1:] {
+					flags[string(flag)] = true
+				}
+			} else {
+				target = arg
+				numTargets++
+			}
+		}
+	}
+
+	if numTargets == 0 {
+		return newIPCMessage("rm: missing operand", utils.Error)
+	} else if numTargets > 1 {
+		return newIPCMessage("rm: too many operands", utils.Error)
+	}
+
+	target = s.expandPath(target)
+
+	if !strings.HasPrefix(target, "/") {
+		target = path.Join(s.Session.WorkingDir, target)
+	}
+
+	if flags["r"] {
+		err := s.Session.Computer.Filesystem.RemoveAll(target)
+		if err != nil && !flags["f"] {
+			message := "Failed to remove directory"
+			if strings.HasSuffix(err.Error(), "no such file or directory") {
+				message = "rm: cannot remove: No such file or directory"
+			}
+			return newIPCMessage(message, utils.Error)
+		}
+	}
+
+	if !flags["r"] {
+		err := s.Session.Computer.Filesystem.Remove(target)
+		if err != nil && !flags["f"] {
+			message := "Failed to remove file"
+			if strings.HasSuffix(err.Error(), "no such file or directory") {
+				message = "No such file or directory"
+			}
+			if strings.HasSuffix(err.Error(), "directory not empty") {
+				message = "Directory not empty"
+			}
+			return newIPCMessage(message, utils.Error)
+		}
+	}
+
+	return newIPCMessage("", utils.SuccessDoNotDisplay)
+}
+
+func (s *Shell) expandPath(target string) string {
+	// expand ~ to home directory
+	target = path.Clean(target)
+	if strings.HasPrefix(target, "~") {
+		if s.Session.CurrentUser == "root" {
+			target = path.Join("/root/", target[1:])
+		} else {
+			target = path.Join("/home/", fmt.Sprintf("%s/", s.Session.CurrentUser), target[1:])
+		}
+	}
+
+	return target
 }
